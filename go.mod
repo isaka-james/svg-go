0b7b19d7ad484b9c5f26352682193ee011e041d6
Masterplan <76619967+isaka-james@users.noreply.github.com>
2024-03-03 20:29:10 +0300

    refreshing version 2

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/isaka-james/svg-go
-
-go 1.21.7
--- test.sh
+++
@@ -1,2 +0,0 @@
-// I am using linux so this is the best way to test the API
-curl -X POST -H "Content-Type: application/json" -d '{"number1": 5, "number2": 3}' http://localhost:8080/api/add
